125c4bcb7cbcd76b98648e8048ac30e301a2fb6e
DUNIA1998 <57006538+DUNIA1998@users.noreply.github.com>
2019-11-30 11:59:09 -0600

    Dibujar Dado

--- Dado/Dado/Program.cs
+++ Dado/Dado/Program.cs
@@ -4,14 +4,42 @@ namespace Dado
 {
     class Program
     {
+        public void dibujarDado(int caraSuperior, int caraFrontal, int caraIzquierda)
+        {
+            //Cara Superior
+            Console.WriteLine("                     ##########          ");
+            Console.WriteLine("                     #        #          ");
+            Console.WriteLine("                     #    {0}   #          ", caraSuperior);
+            Console.WriteLine("                     #        #          ");
+
+            //Caras Frontales
+            Console.WriteLine("#########################################");
+            Console.WriteLine("#         #         #         #         #");
+            Console.WriteLine("#   {0}     #    {1}    #     {2}   #     {3}   #", 7-caraFrontal, 7-caraIzquierda, caraFrontal, caraIzquierda);
+            Console.WriteLine("#         #         #         #         #");
+            Console.WriteLine("#########################################");
+
+            //Cara Inferior
+            Console.WriteLine("                     #        #          ");
+            Console.WriteLine("                     #    {0}   #          ", 7-caraSuperior);
+            Console.WriteLine("                     #        #          ");
+            Console.WriteLine("                     ##########          ");
+
+
+        }
+
         static void Main(string[] args)
         {
+
+            Program variable = new Program();
 
             while (Console.ReadKey().Key == ConsoleKey.Enter)
             {
                 Console.WriteLine("Nueva Visualizacion");
+                variable.dibujarDado(4,5,6);
 
             }
+
 
             Console.ReadLine();
         }
