b53bb9d5aee9c4023997fc0c7553936384730b3f
DUNIA1998 <57006538+DUNIA1998@users.noreply.github.com>
2019-11-30 11:26:06 -0600

    Enter

--- Dado/Dado/Program.cs
+++ Dado/Dado/Program.cs
@@ -7,7 +7,13 @@ namespace Dado
         static void Main(string[] args)
         {
 
-          
+            while (Console.ReadKey().Key == ConsoleKey.Enter)
+            {
+                Console.WriteLine("Nueva Visualizacion");
+
+            }
+
+            Console.ReadLine();
         }
     }
 }
